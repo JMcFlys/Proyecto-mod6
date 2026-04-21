1d7602048d4e30442dfd86a8ecdc4c702818c11a
JMcFlys <josesmcfly@gmail.com>
2026-04-21 13:23:27 -0400

    comentario

---
+++ src/main/java/repository/OrdenRepository.java
@@ -0,0 +1,13 @@
+package repository;
+
+import model.Orden;
+import model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface OrdenRepository extends JpaRepository<Orden, Long> {
+    List<Orden> findByUserOrderByFechaDesc(Long usuarioId);
+    List<Orden> findByProductoId(Long productoId);
+    List<Orden> findByEstado(String estado);
+}
